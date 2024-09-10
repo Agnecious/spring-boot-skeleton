eb20ca5d3225a8c4806c560eddce4dc1853bd8f9
Agnecious <AgneciousNkwana@gmail.com>
2024-09-10 15:36:34 +0200

    Update SimpleSpringBootAppApplication.java

--- SimpleSpringBootApp/src/main/java/com/example/SimpleSpringBootApp/demo/SimpleSpringBootAppApplication.java
+++ SimpleSpringBootApp/src/main/java/com/example/SimpleSpringBootApp/demo/SimpleSpringBootAppApplication.java
@@ -8,6 +8,38 @@ public class SimpleSpringBootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+		connectAndCreateQueue();
+	}
+
+		public static void connectAndCreateQueue() throws UnknownHostException {
+		
+		String rabbitHost =  "openshift-rabbitmq-test-d0400.apps.eu46r.prod.ole.redhat.com";
+		int rabbitPort = 80;
+
+		String rabbitUserName = "guest";
+		String rabbitPassword = "guest";
+		String queueName = "TEST_001";
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(rabbitHost);
+		factory.setPort(rabbitPort);
+		factory.setUsername(rabbitUserName);
+		factory.setPassword(rabbitPassword);
+
+
+		log.info("Initialising AsyncCommsChannel with parameters host={}; port={}; username={}; queueName={}",
+					rabbitHost, rabbitPort, rabbitUserName, queueName);
+
+		try {
+			log.info("Inside");
+			Connection connection = factory.newConnection();
+			Channel initialChannel = connection.createChannel();
+			initialChannel.queueDeclare(queueName, true, false, false, null);
+			log.info("Done - created QUEUE");
+			//this.channel = initialChannel;
+			//initialised = true;
+		} catch (Exception e) {
+			log.error("Exception initialising async channel: ", e);
+		}
 	}
 
 }

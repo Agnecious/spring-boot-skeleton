fecf75bb91f8172e12fb576a8f6d6f06213b391c
Agnecious <AgneciousNkwana@gmail.com>
2024-09-10 15:44:43 +0200

    Update SimpleSpringBootAppApplication.java

--- SimpleSpringBootApp/src/main/java/com/example/SimpleSpringBootApp/demo/SimpleSpringBootAppApplication.java
+++ SimpleSpringBootApp/src/main/java/com/example/SimpleSpringBootApp/demo/SimpleSpringBootAppApplication.java
@@ -13,8 +13,8 @@ public class SimpleSpringBootAppApplication {
 
 		public static void connectAndCreateQueue() throws UnknownHostException {
 		
-		String rabbitHost =  "openshift-rabbitmq-test-d0400.apps.eu46r.prod.ole.redhat.com";
-		int rabbitPort = 80;
+		String rabbitHost =  "https://openshift-rabbitmq-test-d0400.apps.eu46r.prod.ole.redhat.com";
+		int rabbitPort = 5672;
 
 		String rabbitUserName = "guest";
 		String rabbitPassword = "guest";
